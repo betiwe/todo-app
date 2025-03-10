372c2a7b0a316f8e98bc0a873f2f3dbfce796262
“betiwe” <betiwe@yandex.ru>
2025-03-10 15:46:13 +0300

    Initial commit need fix checkbox

---
+++ client/src/App.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+import AddTodo from "./components/AddTodo";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TodoList from "./components/TodoList";
+
+import "./styles/App.css"
+
+const App = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    }
+    setTodos([...todos, newTodo]);
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  const toggleTodo = (id) => {
+    setTodos((todo) => todo.id == id ? {...todo, completed: !todo.completed} : todo)
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <AddTodo onAdd={addTodo} />
+      <TodoList todos={todos} onDelete={deleteTodo} onToggle={toggleTodo} />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
